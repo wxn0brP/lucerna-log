f2c03fb76527d3cfadbcf673a02af495e7f48553
wxn0brP <toja.filiper@gmail.com>
2026-02-25 20:29:00 +0100

    feat: bump deps

--- src/transports/db.ts
+++ src/transports/db.ts
@@ -1,13 +1,17 @@
 import { ValtheraCompatible } from "@wxn0brp/db-core";
 import { Transport, LogEntry } from "../logger";
+import { Collection } from "@wxn0brp/db-core/helpers/collection";
 
 export class ValtheraDBTransport implements Transport {
+    c: Collection;
     constructor(
         public db: ValtheraCompatible,
         public collection = "logs"
-    ) { }
+    ) {
+        this.c = db.c(collection);
+    }
 
     async log(entry: LogEntry): Promise<void> {
-        await this.db.add(this.collection, entry);
+        await this.c.add(entry);
     }
 }
